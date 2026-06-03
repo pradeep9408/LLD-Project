1f0dcf1408129a8ab6333f3b7718c0156aeaee4c
unknown <pradeep.22bce9408@vitapstudent.ac.in>
2026-06-03 16:03:55 +0530

    Updated changes in theatre

--- theatre/Seat.java
+++ theatre/Seat.java
@@ -3,22 +3,12 @@ package theatre;
 public class Seat {
 
     private int seatNumber;
-    private boolean booked;
 
     public Seat(int seatNumber) {
         this.seatNumber = seatNumber;
-        this.booked = false;
     }
 
     public int getSeatNumber() {
         return seatNumber;
-    }
-
-    public boolean isBooked() {
-        return booked;
-    }
-
-    public void bookSeat() {
-        booked = true;
     }
 }
